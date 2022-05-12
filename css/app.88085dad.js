0ea7b1ee2272ccf04e0d79a0d6718ac1533b5cae
Anthony Ribeiro <anthony.ribeiro77@gmail.com>
2022-05-13 00:53:01 +0200

    Updates

---
+++ css/app.88085dad.js
@@ -0,0 +1,24 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/scss/app.scss":
+/*!***************************!*\
+  !*** ./src/scss/app.scss ***!
+  \***************************/
+/***/ (() => {
+
+throw new Error("Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\nModuleBuildError: Module build failed (from ./node_modules/postcss-loader/dist/cjs.js):\nValidationError: Invalid options object. PostCSS Loader has been initialized using an options object that does not match the API schema.\n - options has an unknown property 'config'. These properties are valid:\n   object { postcssOptions?, execute?, sourceMap?, implementation? }\n    at validate (/home/anthony/App/picross/node_modules/schema-utils/dist/validate.js:105:11)\n    at Object.getOptions (/home/anthony/App/picross/node_modules/webpack/lib/NormalModule.js:585:19)\n    at Object.loader (/home/anthony/App/picross/node_modules/postcss-loader/dist/index.js:40:24)\n    at processResult (/home/anthony/App/picross/node_modules/webpack/lib/NormalModule.js:758:19)\n    at /home/anthony/App/picross/node_modules/webpack/lib/NormalModule.js:860:5\n    at /home/anthony/App/picross/node_modules/loader-runner/lib/LoaderRunner.js:400:11\n    at /home/anthony/App/picross/node_modules/loader-runner/lib/LoaderRunner.js:252:18\n    at runNextTicks (node:internal/process/task_queues:61:5)\n    at processImmediate (node:internal/timers:437:9)");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/scss/app.scss"]();
+/******/ 	
+/******/ })()
+;
