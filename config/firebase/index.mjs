c483e8519f40374281fac0baaf3bb9be0ba49572
Anthony Ribeiro <anthony.ribeiro@webedia-group.com>
2023-04-30 02:29:09 +0200

    firebase > fix auth domain

--- config/firebase/index.mjs
+++ config/firebase/index.mjs
@@ -3,7 +3,7 @@ const REGION = 'default-rtdb.europe-west1';
 
 export const firebaseConfig = {
     apiKey: "AIzaSyD5Nr22Zt-eb1hof08Ce_4ycpVYPY87SS0",
-    authDomain: `${PROJECT_ID}-.firebaseapp.com`,
+    authDomain: `${PROJECT_ID}.firebaseapp.com`,
     // The value of `databaseURL` depends on the location of the d
     databaseURL: `https://${PROJECT_ID}-${REGION}.firebasedatabase.app`,
     projectId: `${PROJECT_ID}`,
